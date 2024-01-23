e5af723646a94e2a75791e49459eb0f598052f59
Henry Whitaker <henrywhitaker3@outlook.com>
2024-01-23 12:18:58 +0000

    fmt

--- __tests__/call.test.ts
+++ __tests__/call.test.ts
@@ -17,20 +17,21 @@ describe('call', () => {
 
     const url: string = fetchMock.mock.calls[0][0] as string
     const params: object = fetchMock.mock.calls[0][1] as object
-    expect(url).toBe('https://api.example.com/repos/henrywhitaker3/bongo/dispatches')
+    expect(url).toBe(
+      'https://api.example.com/repos/henrywhitaker3/bongo/dispatches'
+    )
     expect(params).toStrictEqual({
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          Authorization: 'token token'
-        },
-        body: JSON.stringify({
-          event_type: 'some_event',
-          client_payload: {
-            foo: 'bar'
-          }
-        })
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        Authorization: 'token token'
+      },
+      body: JSON.stringify({
+        event_type: 'some_event',
+        client_payload: {
+          foo: 'bar'
+        }
       })
-      
+    })
   })
 })
